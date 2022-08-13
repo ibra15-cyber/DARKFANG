eb47ad92a352ea0cb546596dd8b0b93494839db1
ibra <iasafianu@st.ug.edu.gh>
2022-08-13 14:18:56 +0000

    first commit

---
+++ warehouseManagementProject/warehouseManagementWebApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WarehousemanagementwebappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'warehouseManagementWebApp'
