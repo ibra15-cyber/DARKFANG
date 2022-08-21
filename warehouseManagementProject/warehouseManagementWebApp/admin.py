176af55a193fbedd2ba63a13009b3c4146d46346
ibra <iasafianu@st.ug.edu.gh>
2022-08-21 00:40:35 +0000

    sorting admin

--- warehouseManagementProject/warehouseManagementWebApp/admin.py
+++ warehouseManagementProject/warehouseManagementWebApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from warehouseManagementWebApp.models import Product
+
+# Register your models here.
+admin.site.register(Product) #
